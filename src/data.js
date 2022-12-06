f575404b72eb91c9a0e18a062b5cc080344f7306
nbwong <nicholas.b.wong@gmail.com>
2022-12-05 21:23:27 -0800

    First commit with all code

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const projects = [
+    {
+      title: "airDND",
+      subtitle: "Express.js, React.js, Node.js, Mongoose, MongoDB, API, MERN Framework",
+      description:
+        "App to bring together the Dungeons and Dragons community to create events.",
+      image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "5,6,7,8",
+      subtitle: "Handlebars, API, MVC Framework",
+      description:
+        "This app allows for dancers and organizers to post upcoming dance events in the community!",
+      image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+  ];
+
+  export const skills = [ "Javascript", "React.js", "HTML", "CSS", "Node.js", "Express.js", "mySQL", "Mongoose", "MongoDB", "GitHub" ];
+
+  export const testimonials = [
+    {
+        quote: "This guy was great to work with, really strong back-end knowledge",
+        name: "Kevin U",
+        company: "Mex",
+    },
+    {
+        testimonial: "With the knowledge this guy has, I wonder how far he will go!",
+        name: "Jason K",
+        company: "LabCorp",
+    }
+  ];
